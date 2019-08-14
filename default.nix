aedf240fd8e14937c20411dcd0c62a5208cf5528
Gabriel Volpe <volpegabriel@gmail.com>
2019-08-14 17:59:47 -0500

    Finally fixing Nix build

--- default.nix
+++ default.nix
@@ -1,13 +1,12 @@
 { nixpkgs ? import <nixpkgs> {}, compiler ? "ghc865" }:
 let
   hp = nixpkgs.haskellPackages.override {
-    all-cabal-hashes = nixpkgs.fetchurl {
-      url = "https://github.com/commercialhaskell/all-cabal-hashes/archive/4db96602103463d72e59ce214d0da8af35405f07.tar.gz";
-      sha256 = "14r7m3555ngfafk2wgdj4lnv7zzajcc4j4fiws4k3kr3xwbrwrxr";
-    };
+    overrides =
+      self: super: with nixpkgs.haskell.lib; {
+        primitive = doJailbreak (super.callHackage "primitive" "0.6.3.0" {});
+        refined = super.callHackage "refined" "0.4.2.2" {};
+        transient = super.callHackage "transient" "0.6.3" {};
+      };
   };
-  newTransient = hp.callHackage "transient" "0.6.3" {};
 in
-  hp.callPackage ./rate-limiter.nix {
-    transient = newTransient;
-  }
+  hp.callPackage ./rate-limiter.nix {}
